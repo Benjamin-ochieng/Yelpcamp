b28554321004dba5b55d994a4595150a88e93f8d
Benjamin-ochieng <ochiengbenja@gmail.com>
2019-04-10 20:23:49 +0300

    revert to database connect via url

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const User = require('./models/user');
 const seed = require('./seed');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true } );
+// mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true } );
+mongoose.connect('mongodb+srv://BENJAMIN-OCHIENG:Lcdescfmp4DWZ8Hj@yelpcamp-hacx1.mongodb.net/yelp_camp?retryWrites=true',{ useNewUrlParser: true } );
+app.set('view engine', 'ejs');
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({extended: true}));
 // eslint-disable-next-line no-undef
