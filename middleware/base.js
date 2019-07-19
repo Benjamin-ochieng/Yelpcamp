d8d35c781c95090869687bfa8083bc84ff91e9f9
Benjamin-ochieng <ochiengbenja@gmail.com>
2019-07-19 17:49:21 +0300

    Ui updates

---
+++ middleware/base.js
@@ -0,0 +1,12 @@
+function calculateAverage(reviews) {
+    if (reviews.length === 0) {
+        return 0;
+    }
+    var sum = 0;
+    reviews.forEach(function (element) {
+        sum += element.rating;
+    });
+    return Math.floor(sum / reviews.length)
+}
+
+module.exports = {calculateAverage}
